dfaaafacc0970c7d5b27ec7594be0c3cfa110520
m1gunoff <migunoff21@mail.ru>
2024-06-16 19:11:31 +0300

    WeatherApp

---
+++ constants/index.js
@@ -0,0 +1,33 @@
+export const apiKey = 'c49978c54cc84332a5a63013240801';
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderaterain.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Mist': require('../assets/images/mist.png'),
+    'Light snow': require('../assets/images/snow.png'),
+    'Snow': require('../assets/images/snow.png'),
+    'other': require('../assets/images/snow.png'),
+    'Patchy snow possible': require('../assets/images/snow.png'),
+    'Light freezing rain': require('../assets/images/snow.png'),
+    'Fog': require('../assets/images/mist.png'),
+    'Freezing fog': require('../assets/images/mist.png'),
+    'Heavy snow': require('../assets/images/snow.png'),
+    'Moderate snow': require('../assets/images/snow.png'),
+    'Blizzard': require('../assets/images/mist.png'),
+    'Blowing snow': require('../assets/images/snow.png'),
+    'Moderate or heavy snow showers': require('../assets/images/snow.png'),
+    'Patchy moderate snow': require('../assets/images/snow.png'),
+    'Patchy rain nearby': require('../assets/images/partlycloudy.png')
+}
